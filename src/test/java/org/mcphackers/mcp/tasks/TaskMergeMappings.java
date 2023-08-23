3f33cce9e92dbc66a71ab228b71126cab0a66708
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2023-08-22 23:09:51 -0600

    Am I an idiot?

--- src/test/java/org/mcphackers/mcp/tasks/TaskMergeMappings.java
+++ src/test/java/org/mcphackers/mcp/tasks/TaskMergeMappings.java
@@ -1,6 +1,11 @@
 package org.mcphackers.mcp.tasks;
 
 import org.mcphackers.mcp.MCP;
+import org.mcphackers.mcp.tools.mappings.MappingUtil;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TaskMergeMappings extends TaskStaged {
 	public TaskMergeMappings(MCP instance) {
@@ -11,7 +16,18 @@ public class TaskMergeMappings extends TaskStaged {
 	protected Stage[] setStages() {
 		return new Stage[] {
 				stage(getLocalizedStage("mergemappings"), () -> {
-
+					Path clientMappings = Paths.get("client.tiny");
+					Path serverMappings = Paths.get("server.tiny");
+					Path mergedMappings = Paths.get("merged.tiny");
+					if (Files.exists(clientMappings)) {
+						if (Files.exists(serverMappings)) {
+							// Merge client & server mappings
+							MappingUtil.mergeMappings(clientMappings, serverMappings, mergedMappings);
+						} else {
+							// Only client mappings exist
+							MappingUtil.mergeMappings(clientMappings, mergedMappings);
+						}
+					}
 				})
 		};
 	}
